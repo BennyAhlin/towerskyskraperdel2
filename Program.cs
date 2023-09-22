f8206d4d565ca9d2522080613df72ea92c1d0825
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-09-22 11:44:18 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int realFloor = 114;
+int fakeFloors = 0;
+
+for (int count = 1; count <= realFloor + fakeFloors; count++)
+{
+    if (count % 10 == 4)
+    {
+        fakeFloors++;
+    }
+    if (count % 100 == 13)
+    {
+        fakeFloors++;
+    }
+}
+
+Console.WriteLine(" you floor is " + (realFloor + fakeFloors));
